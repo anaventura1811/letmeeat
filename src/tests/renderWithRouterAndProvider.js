4516ed5c552b4d0eaf70e84f30ce8b6e005cbb87
Ana Ventura <anaventura1811@gmail.com>
2021-07-21 10:20:40 -0300

    feat: configura pasta de testes

---
+++ src/tests/renderWithRouterAndProvider.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+
+const renderWithRouterAndProvider = async (component, route = '/') => {
+	const history = createMemoryHistory();
+	history.push(route);
+
+	return {
+		...render(
+			<Router history={history}>
+				{component}
+			</Router>
+		),
+		history,
+	};
+};
+
+export default renderWithRouterAndProvider;
